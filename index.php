d2968a27fdb7b6e520191f200fc07efdc183e3c3
Akther Hussain <ahabir11@gmail.com>
2014-11-23 15:01:19 -0800

    first commit by source force

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+
+
+
+  <link rel="stylesheet" href="css/runnable.css" />
+  <link rel="stylesheet" href="css/dropzone.css" />
+
+  <script src="dropzone.js"></script>
+
+
+
+</head>
+
+<body>
+  <h1>Drag and Drop files using Dropzone.js</h1>
+
+<form action="upload.php"
+      class="dropzone"
+      id="my-awesome-dropzone"></form>
+      
+
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,21 @@
+<?php
+// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
+// of $_FILES.
+
+$ds          = '/';  //1
+$storeFolder = 'uploads';   //2
+ 
+if (!empty($_FILES)) {
+     
+    $tempFile = $_FILES['file']['tmp_name'];          //3             
+      
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
+     
+    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
+ 
+    move_uploaded_file($tempFile,$targetFile); //6
+     
+}
+
+
+?>
